92af46e68a8368c62c6ad7b1f390407905aca1d2
Sadek Jabr <sadek.jabr@gmail.com>
2018-10-28 12:56:03 +0200

    updated install

--- install.sh
+++ install.sh
@@ -12,15 +12,31 @@ else
    exit 1
 fi
 
-# install sublime
-wget http://c758482.r82.cf2.rackcdn.com/Sublime\ Text\ 2.0.2\ x64.tar.bz2
-tar xvjf Sublime\ Text\ 2.0.2\ x64.tar.bz2
-sudo mv Sublime\ Text\ 2 /opt/
-sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
+# install sublime 3
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get --assume-yes install sublime-text
+
+# install git
+apt-get install git-core
+
+touch /etc/rc.local/codeforpalestine.startup
+sudo echo "" > /etc/rc.local/codeforpalestine.startup
+sudo echo "chromium-browser http://codeforpalestine.herokuapp.com" > /etc/rc.local/codeforpalestine.startup
+
+# clean up
+rm sublimehq-pub.gpg
+
+# install sublime (old ubuntu)
+# wget http://c758482.r82.cf2.rackcdn.com/Sublime\ Text\ 2.0.2\ x64.tar.bz2
+# tar xvjf Sublime\ Text\ 2.0.2\ x64.tar.bz2
+# sudo mv Sublime\ Text\ 2 /opt/
+# sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
 
 # make an icon for unity launcher
-wget https://raw.githubusercontent.com/sadekj/wecodesetup/master/sublime.desktop
-sudo mv sublime.desktop /usr/share/applications/sublime.desktop
+#wget https://raw.githubusercontent.com/sadekj/wecodesetup/master/sublime.desktop
+#sudo mv sublime.desktop /usr/share/applications/sublime.desktop
 
 # clean up!
-rm Sublime\ Text\ 2.0.2\ x64.tar.bz2
+#rm Sublime\ Text\ 2.0.2\ x64.tar.bz2
